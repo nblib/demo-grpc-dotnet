e4f1e7dc71490b03367a321adad593d3750a0bff
hewe <272489473@qq.com>
2018-02-06 14:19:47 +0800

    init

--- demo-grpc-dotnet-server/Program.cs
+++ demo-grpc-dotnet-server/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using demo_grpc_dotnet_server.service;
+using Grpc.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +10,25 @@ namespace demo_grpc_dotnet_server
 {
     class Program
     {
+        const int Port = 50051;
         static void Main(string[] args)
         {
+            HelloTest();
+        }
+        static void HelloTest()
+        {
+            Server server = new Server
+            {
+                Services = { Hello.HelloSerivce.BindService(new HelloServiceImpl()) },
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("gRPC server listening on port " + Port);
+            Console.WriteLine("任意键退出...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
         }
     }
 }
---
+++ demo-grpc-dotnet-server/service/HelloServiceImpl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Hello;
+
+namespace demo_grpc_dotnet_server.service
+{
+    public class HelloServiceImpl : HelloSerivce.HelloSerivceBase
+    {
+        public override Task<HelloReply> tickInfo(HelloRequest request, ServerCallContext context)
+        {
+            string tickinfo = String.Format("name= {0}, age= {1}", "hewe", "44");
+
+            HelloReply reply = new HelloReply { Info = tickinfo, ReceiveTime = DateTime.Now.ToString() };
+            return Task.FromResult(reply);
+        }
+    }
+}
