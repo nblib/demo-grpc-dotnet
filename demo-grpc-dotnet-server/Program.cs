ba064923de62c738e41b541068c0bacb26f59ffa
hewe <272489473@qq.com>
2018-02-06 13:16:28 +0800

    添加项目文件。

---
+++ demo-grpc-dotnet-server/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo_grpc_dotnet_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
